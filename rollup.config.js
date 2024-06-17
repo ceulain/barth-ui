ca6edda225b9bc3e62ffa7e0fbda004c3ad896fd
Ceulain <ceul1barth@hotmail.fr>
2024-06-17 23:56:33 +0200

    feat: add handle css

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
 
@@ -29,8 +30,9 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
+      postcss(),
     ],
-    external: ["react", "react-dom"],
+    external: ["react", "react-dom", /\.css$/],
   },
   {
     input: "src/index.ts",
