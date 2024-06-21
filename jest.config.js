69fd06dc68aba1b76b776fc7787a7e91d10350ce
Ceulain <ceul1barth@hotmail.fr>
2024-06-22 00:17:55 +0200

    feat: add jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   moduleNameMapper: {
     ".(css|less|scss)$": "identity-obj-proxy",
   },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!node_modules/**",
+    "!dist/**",
+    "!src/stories/*,*",
+    "!src/**/*.stories*",
+  ],
+  collectCoverage: true,
+  coverageDirectory: "./coverage/",
+  coverageReporters: ["html"],
 };
